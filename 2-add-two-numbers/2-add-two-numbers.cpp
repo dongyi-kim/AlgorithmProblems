0b75c466c19f46bc4eaea6ce92bcdc4b3138e315
dongyi.kim <waps12b@gmail.com>
2022-04-06 01:57:40 +0900

    Time: 53 ms (44.06%), Space: 71.3 MB (90.20%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -15,32 +15,20 @@ using namespace std;
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<char> zero;
-        for(int i = 0; l1 != nullptr || l2 != nullptr; i++){
-            if(zero.size() <= i){
-                zero.push_back(0);
-            }
+        ListNode* answer = new ListNode(0);
+        ListNode* p = answer;
+        for(int i = 0; p != nullptr; i++, p=p->next){
             if(l1 != nullptr) {
-                zero[i] += l1->val;
+                p->val += l1->val;
                 l1 = l1->next;
             }
             if(l2 != nullptr) {
-                zero[i] += l2->val;
+                p->val += l2->val;
                 l2 = l2->next;
             }
-            if(zero[i] >= 10){
-                zero.push_back(zero[i]/10);
-                zero[i] %= 10;
-            }
-        }
-        ListNode* answer = new ListNode(0);
-        
-        ListNode* p = answer;
-        for(int i = 0; i < zero.size(); i+= 1){
-            p->val = zero[i];
-            if( i + 1 < zero.size()){
-                p->next = new ListNode(0);
-                p = p->next;
+            if(p->val >= 10 || l1 != nullptr || l2 != nullptr){
+                p->next = new ListNode(p->val/10);
+                p->val = p->val%10;
             }
         }
         return answer;
