0831c5658da326dc7ed51b70a19b576bd6690a09
dongyi.kim <waps12b@gmail.com>
2022-04-19 02:02:35 +0900

    Time: 4 ms (25.26%), Space: 5.8 MB (81.39%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,13 +3,28 @@ using namespace std;
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n == 0) return 1;
-        double ret = (abs(n)%2==1) ? x : 1;
-        if( n < 0 ){
-            return 1 / (ret * myPow(x*x, -(n/2) ));   
-        }else{
-            return 1 * ret * myPow(x*x, n/2);   
-        }
-        
+double res = 1;
+
+while (n) {
+
+    if (n % 2 ) {
+
+        if(n > 0)
+
+            res*= x;
+
+        else
+
+            res/= x;
+
+    }
+
+    x = x * x;
+
+    n = n / 2;
+
+}
+
+return res;
     }
 };
