dae11ba7670dc33518fa8f2f9bd90e6817669b4a
dongyi.kim <waps12b@gmail.com>
2022-04-19 02:06:47 +0900

    Time: 4 ms (25.26%), Space: 6 MB (45.62%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,30 +1,30 @@
-#include<cmath>
-using namespace std;
 class Solution {
 public:
-    double myPow(double x, int n) {
-double res = 1;
-
-while (n) {
-
-    if (n % 2 ) {
-
-        if(n > 0)
-
-            res*= x;
-
+    double myPow(double x, int n)
+    {
+         if(n==INT_MIN)
+        {
+            if(x>1) return 0;
+            else return 1;
+        }
+        if(n>=0)
+        {
+            if(n==0) return 1;
+            if(n&1)//odd number
+            return x*myPow(x,n-1); 
+            else
+            return myPow(x*x,n/2);
+        }
         else
-
-            res/= x;
-
+        {  
+            if(n!=INT_MIN)
+            n = -1*n;
+            if(n==0) return 1;
+            if(n&1)
+            return (1/x)*myPow(1/x,n-1);
+            else
+            return myPow(1/(x*x),n/2);
+        }
     }
-
-    x = x * x;
-
-    n = n / 2;
-
-}
-
-return res;
-    }
+    
 };
